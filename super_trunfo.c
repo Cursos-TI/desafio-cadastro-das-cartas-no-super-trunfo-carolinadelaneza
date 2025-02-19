cab3c6e8855e07b4009274a383eb5d3883026eaf
carolinadelaneza <202502141556@alunos.estacio.br>
2025-02-19 01:50:42 +0000

    alteracoes print e scanf

--- super_trunfo.c
+++ super_trunfo.c
@@ -6,15 +6,31 @@ int main () {
     float area;
     float pib;
     char cidade [100];
-    char estado;
-    char cod_carta [15];
+    char estado[1];
+    char cod_carta [5];
+    int carta;
     int pontos_turisticos;
     
-    printf("Nome da cidade: %s\n", cidade);
-    printf("População: %d\n", populacao);
-    printf("Área: %.2f km²\n", area);
-    printf("PIB: R$ %.2f\n", pib);
-    printf("Número de pontos turísticos: %d\n", pontos_turisticos);
+    printf("Nome da cidade:");
+    scanf ("%s", cidade);
+
+    printf("População:");
+    scanf ("%d", &populacao);
+
+    printf("Área: ");
+    scanf ("%f", &area);
+
+    printf("PIB: R$");
+    scanf ("%f", &pib);
+
+    printf("Número de pontos turísticos:");
+    scanf ("%d", &pontos_turisticos);
+
+    printf("Estado:(somente uma letra)");
+    scanf("%s", estado);
+
+    printf("Código da carta: (ex;a01,b03): ");
+    scanf ("%s", cod_carta);
 
     return 0;
 }
